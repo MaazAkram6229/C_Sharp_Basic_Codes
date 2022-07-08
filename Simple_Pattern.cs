93f563a831367bef65ff5fc63f1dc1562fc66282
MaazAkram6229 <108363439+MaazAkram6229@users.noreply.github.com>
2022-07-08 10:58:35 +0500

    Printing a simple pattern

---
+++ Simple_Pattern.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i < 10; i = i + 2)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i < 15; i = i + 3)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.Read();
+        }
+
+    }
+}
