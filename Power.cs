5e037c028bae804c1ae6379b8209b1bbb5760000
MaazAkram6229 <108363439+MaazAkram6229@users.noreply.github.com>
2022-07-08 11:06:10 +0500

    Finding power from recursion

---
+++ Power.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numnber");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter power");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PowerValue(a, b));
+        }
+
+        static int PowerValue(int num, int power)
+        {
+
+            if (power != 0)
+            {
+                return num * PowerValue(num, power - 1);
+            }
+            return 1;
+
+
+        }
+    }
+
+
+}
