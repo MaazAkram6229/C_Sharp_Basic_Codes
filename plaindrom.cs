06d9f30e63e900cd1798d899a8e037886a45300e
MaazAkram6229 <108363439+MaazAkram6229@users.noreply.github.com>
2022-07-08 11:09:51 +0500

    Checking if number is palindrome

---
+++ plaindrom.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Anser is {0}", isPalindrom(inp));
+        }
+        static bool isPalindrom(int num)
+        {
+            string a = Convert.ToString(num);
+            int len = a.Length;
+            int i = 0;
+            for (i = 0; i < len - 1; i++)
+            {
+                if (a[i] == a[len - i - 1])
+                {
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            if (i == len - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
