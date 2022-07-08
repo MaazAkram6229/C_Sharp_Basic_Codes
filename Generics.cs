446b018d36ab09767f936a9959d113d7e862859f
MaazAkram6229 <108363439+MaazAkram6229@users.noreply.github.com>
2022-07-08 11:34:12 +0500

    Generics Basic Understanding

---
+++ Generics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Gen<String> Chai = new Gen<string>();
+            Chai.Cup = "Maaz";
+
+            Gen<int> pani = new Gen<int>();
+            pani.Cup = 23;
+
+            Console.WriteLine("CHAI IS " + Chai.Cup);
+
+            Console.WriteLine("PANI IS " + pani.Cup);
+        }
+    }
+    public class Gen<T> 
+    {
+        private T cup;
+        public T Cup
+        {
+            get { return cup; }
+            set { cup = value; }
+        }
+
+    }
+}
