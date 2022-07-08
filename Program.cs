4f55a7917463aadf4fb2b1e8e0271cf1951043e4
MaazAkram6229 <108363439+MaazAkram6229@users.noreply.github.com>
2022-07-08 10:51:31 +0500

    A program to extract after decimal part of a float

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input;
+            Console.WriteLine("Enter the input");
+            input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Returns : {0}", func(input));
+        }
+        static int func(double inp)
+        {
+            int ans = (int)(100 * (inp - ((double)((int)Math.Truncate(inp)))));
+            return ans;
+        }
+        static int func2(double inp)
+        {
+            double ans = (inp - Math.Truncate(inp));
+            return Convert.ToInt32(ans);
+        }
+    }
+}
